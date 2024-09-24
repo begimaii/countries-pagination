01e53af36e56cc3c1d7c7610e554e779f29ec4b1
Begimai Abdybekova <b.abdybekova@gmail.com>
2024-09-23 19:35:33 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const totalCountEl = document.getElementById("total-count");
+const pageCountEl = document.getElementById("page-count");
+const searchEl = document.getElementById("search");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("previous");
+const countriesListEl = document.querySelector(".countries");
